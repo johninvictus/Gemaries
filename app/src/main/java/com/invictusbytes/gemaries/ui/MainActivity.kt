4840144b97d97c7787c35cc7fbc9a17dbb14e7ee
johninvictus <invictusnitude@gmail.com>
2019-08-30 03:40:00 +0300

    added initial main activity design

--- app/src/main/java/com/invictusbytes/gemaries/ui/MainActivity.kt
+++ app/src/main/java/com/invictusbytes/gemaries/ui/MainActivity.kt
@@ -1,13 +1,29 @@
 package com.invictusbytes.gemaries.ui
 
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.invictusbytes.gemaries.R
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : DaggerAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupToolbar()
+        setBg()
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(toolbar)
+    }
+
+    private fun setBg() {
+        Glide.with(this)
+            .load(R.drawable.bakery)
+            .into(bgBaker)
     }
 }
