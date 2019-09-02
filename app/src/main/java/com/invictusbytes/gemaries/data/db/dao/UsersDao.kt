b56716bb65a478813b51dbda0de3eaca0057b925
johninvictus <invictusnitude@gmail.com>
2019-09-02 14:41:53 +0300

    added getunassigned users

--- app/src/main/java/com/invictusbytes/gemaries/data/db/dao/UsersDao.kt
+++ app/src/main/java/com/invictusbytes/gemaries/data/db/dao/UsersDao.kt
@@ -1,8 +1,7 @@
 package com.invictusbytes.gemaries.data.db.dao
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
+import androidx.lifecycle.LiveData
+import androidx.room.*
 import com.invictusbytes.gemaries.data.db.entities.UsersEntity
 
 @Dao
@@ -11,4 +10,15 @@ interface UsersDao {
     @Insert(onConflict = OnConflictStrategy.ABORT)
     fun addUser(usersEntity: UsersEntity): Long
 
+
+    @SuppressWarnings(RoomWarnings.CURSOR_MISMATCH)
+    @Query(
+        "SELECT * FROM users u " +
+                "LEFT JOIN Assigned a" +
+                " ON u.id = a.user_id " +
+                "WHERE a.crate_id IS NULL" +
+                " OR a.active = :active" +
+                " ORDER BY u.created DESC"
+    )
+    fun getUnAssignedUsers(active: Boolean): LiveData<UsersEntity>
 }
