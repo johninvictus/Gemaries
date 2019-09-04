9650e1b3436cfabc4dcd426672940a6eb2810a49
johninvictus <invictusnitude@gmail.com>
2019-09-04 07:51:44 +0300

    added clients unassigned viewmodel

---
+++ app/src/main/java/com/invictusbytes/gemaries/ui/unassigned_clients/UnAssignedClientsViewModel.kt
@@ -0,0 +1,8 @@
+package com.invictusbytes.gemaries.ui.unassigned_clients
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class UnAssignedViewModel @Inject constructor(): ViewModel(){
+
+}
